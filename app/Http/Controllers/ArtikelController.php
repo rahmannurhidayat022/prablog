ef7ec3b1885db242adf06b41a13dc8b16680e4e8
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-25 20:25:45 +0700

    membuat controller create

---
+++ app/Http/Controllers/ArtikelController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Artikel;
+
+class ArtikelController extends Controller
+{
+    public function index() {
+        $rows = Artikel::all();
+
+        return view('home', compact('rows'));
+    }
+
+    public function store(Request $request) {
+        $file = $request->file('gambar');
+
+        // Mendapatkan Nama File
+        $nama_file = $file->getClientOriginalName();
+
+        // Mendapatkan Extension File
+        $extension = $file->getClientOriginalExtension();
+
+        // Mendapatkan Ukuran File
+        $ukuran_file = $file->getSize();
+
+        // Proses Upload File
+        $destinationPath = public_path() . 'uploads/';
+        $file->move($destinationPath,$file->getClientOriginalName());
+
+        $dataBaru = Artikel::create([
+            'nama' => $request -> nama,
+            'gambar' => $nama_file,
+            'kota' => $request -> deskripsi,
+            'deskripsi' => $request -> deskripsi,
+            'kategori' => $request -> kategori,
+        ]);
+
+        return redirect()->back();
+    }
+}
