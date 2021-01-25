d0a0851d708755215a6a2f3cf68fc54e39e2ca4f
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-25 20:13:50 +0700

    setting Model Artikel

---
+++ database/migrations/2021_01_25_130855_create_artikel.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('gambar');
+            $table->string('kota');
+            $table->text('deskripsi');
+            $table->string('kategori');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
